ce9cdf85540bd8c282f8496148eac4a222c1b101
DeanxinLuo <88123885+DeanxinLuo@users.noreply.github.com>
2021-09-11 20:48:29 +1000

    Add files via upload

--- sql_queries.py
+++ sql_queries.py
@@ -1,9 +1,9 @@
 import configparser
 
-
 # CONFIG
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
+s3_role = config.get('IAM_ROLE','ARN')
 
 # DROP TABLES
 
@@ -115,17 +115,17 @@ time_table_create = ("""
 
 staging_events_copy = ("""
     COPY staging_events FROM {}
-    CREDENTIALS 'aws_iam_role={}'
+    CREDENTIALS 'aws_iam_role={s3_role}'
     JSON {} 
     COMPUPDATE OFF REGION 'us-west-2';
-""").format()
+""")
 
 staging_songs_copy = ("""
     COPY staging_songs FROM {}
-    CREDENTIALS 'aws_iam_role={}'
+    CREDENTIALS 'aws_iam_role={s3_role}'
     JSON 'auto'
     COMPUPDATE OFF REGION 'us-west-2';
-""").format()
+""")
 
 # FINAL TABLES
 
@@ -257,7 +257,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create,time_table_create,song_table_create,artist_table_create,songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
